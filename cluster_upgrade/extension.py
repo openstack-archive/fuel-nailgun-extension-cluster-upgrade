openstack-archive/fuel-web

Directly assign node to an upgrade cluster
The patch adds method that assigns a node to an upgrade cluster without
deleting it from DB. This allows to keep ID of the node and IP addresses
assigned to it. The node is booted into the bootstrap image as soon as
it moves to an upgrade cluster.

Implements blueprint: nailgun-api-env-upgrade-extensions

Change-Id: If10fadd149a32317420778607146d9d12108d3f9

nailgun/nailgun/extensions/upgrades/extension.py
@@ -28,6 +28,8 @@ class UpgradesExtention(extensions.BaseExtension):
     urls = [
         {'uri': r'/clusters/(?P<cluster_id>\d+)/upgrade/start/?$',
          'handler': handlers.ClusterUpgradeHandler},
+        {'uri': r'/clusters/(?P<cluster_id>\d+)/upgrade/relocate/?$',
+         'handler': handlers.NodeRelocateHandler},
     ]
 
     @classmethod

openstack-archive/fuel-nailgun-extension-cluster-upgrade

Co-Authored-By: Artur Svechnikov <asvechnikov@mirantis.com>

cluster_upgrade/extension.py
@@ -28,6 +28,8 @@ class ClusterUpgradeExtension(extensions.BaseExtension):
     urls = [
         {'uri': r'/clusters/(?P<cluster_id>\d+)/upgrade/clone/?$',
          'handler': handlers.ClusterUpgradeHandler},
+        {'uri': r'/clusters/(?P<cluster_id>\d+)/upgrade/assign/?$',
+         'handler': handlers.NodeReassignHandler},
     ]
 
     @classmethod