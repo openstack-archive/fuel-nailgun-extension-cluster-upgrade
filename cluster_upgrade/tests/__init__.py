d4db5ba78ccefd08e465cd30116094678c5cb35f
Nikita Zubkov <nzubkov@mirantis.com>
2016-07-13 13:43:58 +0300

    Fix package namespace

--- cluster_upgrade/tests/__init__.py
+++ cluster_upgrade/tests/__init__.py
@@ -14,4 +14,4 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-EXTENSION = "nailgun.extensions.cluster_upgrade."
+EXTENSION = "cluster_upgrade."
