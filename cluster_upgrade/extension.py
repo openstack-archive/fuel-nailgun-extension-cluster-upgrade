openstack-archive/fuel-web

Introduce extension to upgrade clusters
The patch adds an extension which implements the procedure of upgrading
clusters from one major release to another. As a first step of the
procedure the extension provides an ability to create a seed cluster
with the same settings as the original.

Implements blueprint: nailgun-api-env-upgrade-extensions

Change-Id: I22d51a3ffd51a7c88bdcbde0eef6f47b65def1c8

nailgun/nailgun/extensions/cluster_upgrade/extension.py
@@ -1,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+
+from nailgun import extensions
+
+from . import handlers
+
+
+class ClusterUpgradeExtention(extensions.BaseExtension):
+    name = 'cluster_upgrade'
+    version = '0.0.1'
+
+    urls = [
+        {'uri': r'/clusters/(?P<cluster_id>\d+)/upgrade/clone/?$',
+         'handler': handlers.ClusterUpgradeHandler},
+    ]
+
+    @classmethod
+    def alembic_migrations_path(cls):
+        return os.path.join(os.path.dirname(__file__),
+                            'alembic_migrations', 'migrations')
+
+    @classmethod
+    def on_cluster_delete(cls, cluster):
+        from .objects import relations
+
+        relations.UpgradeRelationObject.delete_relation(cluster.id)

nailgun/nailgun/extensions/cluster_upgrade/objects/relations.py
@@ -1,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from nailgun.db import db
+
+from .. import models
+
+
+class UpgradeRelationObject(object):
+    @staticmethod
+    def _query_cluster_relations(cluster_id):
+        return db.query(models.UpgradeRelation).filter(
+            (models.UpgradeRelation.orig_cluster_id == cluster_id) |
+            (models.UpgradeRelation.seed_cluster_id == cluster_id))
+
+    @classmethod
+    def delete_relation(cls, cluster_id):
+        cls._query_cluster_relations(cluster_id).delete()
+
+    @classmethod
+    def is_cluster_in_upgrade(cls, cluster_id):
+        relation = cls._query_cluster_relations(cluster_id).first()
+        return bool(relation)
+
+    @classmethod
+    def create_relation(cls, orig_cluster_id, seed_cluster_id):
+        relation = models.UpgradeRelation(
+            orig_cluster_id=orig_cluster_id,
+            seed_cluster_id=seed_cluster_id)
+        db.add(relation)
+        db.flush()

openstack-archive/fuel-nailgun-extension-cluster-upgrade

cluster_upgrade/extension.py
@@ -1,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+
+from nailgun import extensions
+
+from . import handlers
+
+
+class ClusterUpgradeExtension(extensions.BaseExtension):
+    name = 'cluster_upgrade'
+    version = '0.0.1'
+
+    urls = [
+        {'uri': r'/clusters/(?P<cluster_id>\d+)/upgrade/clone/?$',
+         'handler': handlers.ClusterUpgradeHandler},
+    ]
+
+    @classmethod
+    def alembic_migrations_path(cls):
+        return os.path.join(os.path.dirname(__file__),
+                            'alembic_migrations', 'migrations')
+
+    @classmethod
+    def on_cluster_delete(cls, cluster):
+        from .objects import relations
+
+        relations.UpgradeRelationObject.delete_relation(cluster.id)

cluster_upgrade/objects/relations.py
@@ -1,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from nailgun.db import db
+
+from .. import models
+
+
+class UpgradeRelationObject(object):
+    @staticmethod
+    def _query_cluster_relations(cluster_id):
+        return db.query(models.UpgradeRelation).filter(
+            (models.UpgradeRelation.orig_cluster_id == cluster_id) |
+            (models.UpgradeRelation.seed_cluster_id == cluster_id))
+
+    @classmethod
+    def get_cluster_relation(cls, cluster_id):
+        return cls._query_cluster_relations(cluster_id).first()
+
+    @classmethod
+    def delete_relation(cls, cluster_id):
+        cls._query_cluster_relations(cluster_id).delete()
+
+    @classmethod
+    def is_cluster_in_upgrade(cls, cluster_id):
+        query = cls._query_cluster_relations(cluster_id).exists()
+        return db.query(query).scalar()
+
+    @classmethod
+    def create_relation(cls, orig_cluster_id, seed_cluster_id):
+        relation = models.UpgradeRelation(
+            orig_cluster_id=orig_cluster_id,
+            seed_cluster_id=seed_cluster_id)
+        db.add(relation)
+        db.flush()