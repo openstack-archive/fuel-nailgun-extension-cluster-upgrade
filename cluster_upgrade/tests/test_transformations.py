d3cd0bf94f8fde685add97737b7cf98d2ba97e67
Dmitry Nikishov <dnikishov@mirantis.com>
2016-12-07 11:45:57 +0400

    Set vip_namespace when migrating vips from 6.x
    Change-Id: I7d76f7984a1870c070126b0ccde160f320576cf3

--- cluster_upgrade/tests/test_transformations.py
+++ cluster_upgrade/tests/test_transformations.py
@@ -247,17 +247,21 @@ class TestClusterTransformers(nailgun_test_base.BaseUnitTest):
 
 class TestVipTransformers(nailgun_test_base.BaseUnitTest):
     def setUp(self):
-        ip = '0.0.0.0'
+        vip_dict = {'ip_addr': '0.0.0.0'}
+        new_haproxy_vip_dict = {'ip_addr': '0.0.0.0',
+                                'vip_namespace': 'haproxy'}
+        new_vrouter_vip_dict = {'ip_addr': '0.0.0.0',
+                                'vip_namespace': 'vrouter'}
         self.data = {
             1: {
-                'haproxy': ip,
-                'vrouter': ip,
-                'test': ip,
+                'haproxy': vip_dict,
+                'vrouter': vip_dict,
+                'test': vip_dict,
             },
             2: {
-                'haproxy': ip,
-                'vrouter': ip,
-                'test': ip,
+                'haproxy': vip_dict,
+                'vrouter': vip_dict,
+                'test': vip_dict,
             }
         }
         self.mapping = {1: 'management', 2: 'public'}
@@ -269,11 +273,11 @@ class TestVipTransformers(nailgun_test_base.BaseUnitTest):
         self.assertEqual(
             data, ({
                 1: {
-                    'management': ip,
-                    'vrouter': ip,
+                    'management': new_haproxy_vip_dict,
+                    'vrouter': new_vrouter_vip_dict,
                 },
                 2: {
-                    'public': ip,
-                    'vrouter_pub': ip,
+                    'public': new_haproxy_vip_dict,
+                    'vrouter_pub': new_vrouter_vip_dict,
                 }}, {1: 'management', 2: 'public'})
         )
